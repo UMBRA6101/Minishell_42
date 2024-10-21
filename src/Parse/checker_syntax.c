fdfe70d669fd618e479a5fe80729f221b11a0df8
UMBRA6101 <thomass.dossantos@icloud.com>
2024-10-21 05:52:59 +0200

    error quote

--- src/Parse/checker_syntax.c
+++ src/Parse/checker_syntax.c
@@ -13,23 +13,21 @@ static int	occ_quote(char *buff)
 
 static int	quote_check(char *word, t_erreur *err, int i, const int nb_word)
 {
-	if ((ft_strlen(word) <= 2) || (i + 1) == nb_word)
+	if ((ft_strlen(word) <= 2) || (((i + 1) == nb_word) && i != 0))
 	{
 		if ((i == 0) || (word[0] == word[1]))
 			err->error_code = SYNTAX;
-		printf("ouai tqt\n");
-		printf("i : %d || nb_word : %d\n", i + 1, nb_word);
 		if ((i + 1) == nb_word)
 			err->error_code = STX_QUOTE;
+		return (-1);
 	}
-	else if (occ_quote(word) > 2)
+	else if (occ_quote(word) >= 2)
 	{
 		err->c = word[0];
 		err->error_code = D_SYNTAX;
 		return (-1);
 	}
-	else
-		return (-1);
+	return (0);
 }
 
 int	syntax_check(t_split *split, const int nb_word, t_erreur *err)
@@ -39,6 +37,7 @@ int	syntax_check(t_split *split, const int nb_word, t_erreur *err)
 	char sep;
 
 	i = 0;
+	sw = 0;
 	while (i < nb_word)
 	{
 		sep = split[i].word[0];
@@ -51,7 +50,7 @@ int	syntax_check(t_split *split, const int nb_word, t_erreur *err)
 			}
 			else if (sep == '>' || sep == '<')
 			{
-				if (quote_check(split[i].word, err, i, nb_word) >= 0)
+				if (quote_check(split[i].word, err, i, nb_word) == -1)
 					return (-1);
 			}
 			sw = 1;
